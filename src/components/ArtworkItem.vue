83456c2838fc42bd9ff863a051efc213af044159
maxim <tyminko@gmail.com>
2017-11-02 18:02:11 +0100

    fix Image size in Thumb

--- src/components/ArtworkItem.vue
+++ src/components/ArtworkItem.vue
@@ -3,7 +3,7 @@
     <router-link :to="uri" class="item-wrap">
       <div class="item-image-wrap">
         <img v-if="artwork.thumbUrl" :src="artwork.thumbUrl" class="item-image">
-        <iframe v-else-if="artwork.url" :src="artwork.url" class="work-preview"></iframe>
+        <!--<iframe v-else-if="artwork.url" :src="artwork.url" class="work-preview"></iframe>-->
         <img v-else src="/static/img/no-preview.png" class="item-image">
       </div>
       <div class="item-content">
